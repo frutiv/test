22b7e0bbd420c8dd940aebe2527235d829d7ebe8
frutiv <97821947+frutiv@users.noreply.github.com>
2022-01-23 10:31:17 +0300

    Add files via upload

---
+++ Новая папка (2)/scrip.js
@@ -0,0 +1,37 @@
+//var ei = document.querySelector("#JS")
+//var id = document.querySelector("#btn")
+
+//id.onclick = function() {
+//	ei.innerHTML = "f[f[f"
+//	ei.style.color = "red"
+//}
+//var fg = document.querySelector("#rob")
+//var fh = document.querySelector("#css")
+
+//fh.onclick = function() {
+//	fh.innerHTML = "f[f[f"
+//	fh.style.background = "blue"
+//}
+//console.log(fg)
+
+
+var age = +prompt("Сколько вам лет?")
+
+// if (age == 0) {
+// 	alert(" Интернет в 0 лет")
+// }
+// else if(age < 18) {
+//      alert("Тебе нельзя")
+// }
+// else {
+// 	alert("Привет незнакомец")
+// }
+	// switch (age){
+	// 	case 0: alert("Интернет в 0 лет")
+	// 			break;
+	// 	case 18: alert("Тебе нельзя")
+	// 			break;
+	// 	default: alert("Привет незнакомец")
+	// 			break;
+	age < 18 ? alert("Иди погуляй") : 
+	age = 20 ? alert("Тебе 20") : alert("Привет незнакомец")
